cecf99e8f461bf6ca333521a80c4f34b5d8ea521
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-05-28 00:52:13 +0900

    fix : 유저관리 페이지 UI개발

--- src/main/java/com/example/bread/service/UserService.java
+++ src/main/java/com/example/bread/service/UserService.java
@@ -5,11 +5,13 @@ import com.example.bread.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
