ef131bae8add8a417b203c6e7e1a673b02c1a826
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-08-30 16:11:17 +0900

    fix : 타임리프 에러 수정

---
+++ dump.sql
@@ -0,0 +1,3 @@
+/* admin계정 */
+insert into user_info(address, birth_date, email, name, nick_name, password, phone_number, role, user_id)
+  values ('서울시', '2000-01-01', 'admin@hyeokbread.co.kr', 'admin', '관리자', '$2a$10$sAqk0.PwY3rIu20BLkFqne7q.N2DmXqa5aFxJWpe23Dz1Y5GM7Kq2', '010-0000-0000', 'admin', 'admin')
