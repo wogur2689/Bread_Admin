3f2309c9548bcb929e56b9c4e8b846f9cc8fc9e1
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-11-20 16:06:21 +0900

    feat : AuditorAwareConfig

--- src/main/java/com/example/bread/common/entity/TimeEntity.java
+++ src/main/java/com/example/bread/common/entity/TimeEntity.java
@@ -4,25 +4,30 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
+
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeEntity {
     @CreatedDate
-    @Column(name = "created_at", updatable = false)
+    @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @Column(name = "created_by", length = 10, nullable = true)
+    @CreatedBy
+    @Column(name = "created_by", length = 10, nullable = false)
     private String createdBy; // 작성자
 
-    @Column(name = "updated_by", length = 10, nullable = false)
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 10)
     private String updatedBy; // 수정자
 }
---
+++ src/main/java/com/example/bread/config/AuditorAwareConfig.java
@@ -0,0 +1,30 @@
+package com.example.bread.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Slf4j
+@Configuration
+public class AuditorAwareConfig implements AuditorAware<String> {
+
+    /**
+     * 생성자 및 수정자 추적 설정
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        /**
+         * SecurityContext 에서 인증정보를 가져와 주입.
+         */
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(authentication -> {
+                    final Principal loginUser = (Principal) authentication.getPrincipal();
+                    log.info("user : {}", loginUser.getName());
+                    return loginUser.getName();
+                });
+    }
+}
