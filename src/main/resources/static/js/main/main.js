38db8ae535cf6d50780969831771414d4c76004e
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-12-02 16:03:46 +0900

    fix : 차트 한국어로 변경

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -3,9 +3,9 @@ const userCtx = document.getElementById('userChart').getContext('2d');
 new Chart(userCtx, {
     type: 'bar',
     data: {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        labels: ['월', '화', '수', '목', '금', '토', '일'],
         datasets: [{
-            label: 'Users',
+            label: '사용자',
             data: [12, 19, 3, 5, 2, 3, 7],
             backgroundColor: '#d9b382',
         }]
@@ -17,9 +17,9 @@ const salesCtx = document.getElementById('salesChart').getContext('2d');
 new Chart(salesCtx, {
     type: 'line',
     data: {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        labels: ['월', '화', '수', '목', '금', '토', '일'],
         datasets: [{
-            label: 'Sales',
+            label: '매출액',
             data: [100, 200, 150, 300, 250, 400, 350],
             borderColor: '#8b5a2b',
             backgroundColor: 'rgba(139, 90, 43, 0.2)',
