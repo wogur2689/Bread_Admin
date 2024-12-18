6990f787ace27eceac0ce4a72ab7f3f8d1110e07
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-12-19 01:09:46 +0900

    fix : 권한관리 테이블 제거 및 점장 권한 추가

--- src/main/java/com/example/bread/web/user/entiry/UsersEntity.java
+++ src/main/java/com/example/bread/web/user/entiry/UsersEntity.java
@@ -4,6 +4,9 @@ import com.example.bread.common.entity.TimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 유저관리
  */
@@ -20,30 +23,30 @@ public class UsersEntity extends TimeEntity {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false, length = 20)
     private String userId;      //아이디
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false, length = 20)
     private String password;    //비밀번호
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, length = 20)
     private String name;        //이름
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, length = 50)
     private String email;       //이메일
 
-    @Column(name = "nick_name")
+    @Column(name = "nick_name", nullable = false, length = 20)
     private String nickName;    //닉네임
 
-    @Column(name = "birth_date")
+    @Column(name = "birth_date", nullable = false, length = 20)
     private String birthDate;   //생년월일(yyyy-mm-dd)
 
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", nullable = false, length = 20)
     private String phoneNumber; //전화번호(010-0000-0000)
 
-    @Column(name = "address")
+    @Column(name = "address", nullable = false, length = 100)
     private String address;     //주소
 
-    @Column(name = "role")
+    @Column(name = "role", nullable = false, length = 20)
     private String role;        //권한 (기본은 guest)
 }
--- src/main/java/com/example/bread/web/user/util/Role.java
+++ src/main/java/com/example/bread/web/user/util/Role.java
@@ -10,8 +10,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Role {
     USER("user", "사용자"),
+    BRANCH("branch", "점장"),
     ADMIN("admin", "관리자");
 
     private final String role;
     private final String msg;
+    /**
+     * 사용자 = 빵을 구매하는 사람
+     * 점장 = 빵을 판매하는 사람
+     * 관리자 = 시스템을 관리하는 사람
+     */
 }
