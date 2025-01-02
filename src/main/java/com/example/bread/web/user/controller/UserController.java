054f0ddb86a04240cf585879952e3b2014d2040b
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-01-03 00:07:27 +0900

    fix : 유저 목록 페이징 적용

--- src/main/java/com/example/bread/web/user/controller/UserController.java
+++ src/main/java/com/example/bread/web/user/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.bread.web.user.controller;
 
+import com.example.bread.web.board.entity.BoardEntity;
+import com.example.bread.web.user.entiry.UsersEntity;
 import com.example.bread.web.user.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
@@ -22,8 +26,11 @@ public class UserController {
      * 유저 관리 화면
      */
     @GetMapping("/userList")
-    public ModelAndView user(ModelAndView mav) {
-        mav.addObject("userList", usersService.getUserList());
+    public ModelAndView list(ModelAndView mav, @RequestParam(defaultValue = "1") int page) {
+        Page<UsersEntity> response = usersService.getUserList(page);
+        mav.addObject("response", response);
+        mav.addObject("currentPage", page);
+        mav.addObject("totalPages", response.getTotalPages());
         mav.setViewName("user/user_list");
         return mav;
     }
--- src/main/java/com/example/bread/web/user/service/UsersService.java
+++ src/main/java/com/example/bread/web/user/service/UsersService.java
@@ -4,10 +4,12 @@ import com.example.bread.web.user.entiry.UsersEntity;
 import com.example.bread.web.user.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Slf4j
 @Service
@@ -19,7 +21,8 @@ public class UsersService {
     /**
      * 유저 목록 반환(추후 페이징 적용)
      */
-    public List<UsersEntity> getUserList() {
-        return usersRepository.findAll();
+    public Page<UsersEntity> getUserList(int page) {
+        Pageable pageable = PageRequest.of(page - 1, 10, Sort.by(Sort.Direction.DESC, "createdAt"));
+        return usersRepository.findAll(pageable);
     }
 }
