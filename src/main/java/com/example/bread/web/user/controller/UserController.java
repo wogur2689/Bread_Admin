8de550970b05fb20d870b05ce2953463689ec43b
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-03-03 19:27:37 +0900

    feat : QueryDsl 적용

--- src/main/java/com/example/bread/web/user/controller/UserController.java
+++ src/main/java/com/example/bread/web/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.bread.web.user.controller;
 
-import com.example.bread.web.board.entity.BoardEntity;
 import com.example.bread.web.user.entiry.UsersEntity;
 import com.example.bread.web.user.service.UsersService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +27,19 @@ public class UserController {
      */
     @GetMapping("/userList")
     public ModelAndView list(ModelAndView mav, @RequestParam(defaultValue = "1") int page) {
+        Page<UsersEntity> response = usersService.getUserList(page);
+        mav.addObject("response", response);
+        mav.addObject("currentPage", page);
+        mav.addObject("totalPages", response.getTotalPages());
+        mav.setViewName("user/user_list");
+        return mav;
+    }
+
+    /*
+     * 유저 Excel upload
+     */
+    @PostMapping("/userList")
+    public ModelAndView excelUpload(ModelAndView mav, @RequestParam(defaultValue = "1") int page) {
         Page<UsersEntity> response = usersService.getUserList(page);
         mav.addObject("response", response);
         mav.addObject("currentPage", page);
--- src/main/java/com/example/bread/web/user/dto/LoginDto.java
+++ src/main/java/com/example/bread/web/user/dto/LoginDto.java
@@ -1,9 +1,18 @@
 package com.example.bread.web.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * 기본 로그인 dto
  */
-public record LoginDto (
-    String userId,  //userID
-    String role     //role
-) {}
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String userId;  //userID
+    private String role;    //role
+}
