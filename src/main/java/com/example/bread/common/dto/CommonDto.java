84049efaef83b0dfe91210b0e2a4d866e30af0b9
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-03-18 23:47:18 +0900

    fix : commonDto record -> class

--- src/main/java/com/example/bread/common/dto/CommonDto.java
+++ src/main/java/com/example/bread/common/dto/CommonDto.java
@@ -1,9 +1,7 @@
 package com.example.bread.common.dto;
 
-
-//java 14에서 부터 record 사용가능
-public record CommonDto(
-    String code, //결과코드
-    String msg,  //반환메세지
-    Object data  //데이터
-){}
+public class CommonDto<T> {
+    private String code; //결과코드
+    private String msg;  //반환메세지
+    private T data; //데이터
+}
