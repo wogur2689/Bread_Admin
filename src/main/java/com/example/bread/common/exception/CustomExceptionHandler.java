eb4480492ea8bcbd4e91a3c096608fc24a7a56c0
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-03-23 01:30:26 +0900

    fix : CustomExceptionHandler 수정

--- src/main/java/com/example/bread/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/bread/common/exception/CustomExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.example.bread.common.exception;
 
-import com.example.bread.common.dto.CommonDto;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<CommonDto> handleCustomException(CustomException ex) {
-        return ResponseEntity.status(500).body(new CommonDto(ex.getCode(), ex.getMsg(), null));
+    @ExceptionHandler({CustomException.class})
+    public ModelAndView handleCustomException(CustomException ex) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("code", ex.getCode());
+        mav.addObject("msg", ex.getMsg());
+        mav.setViewName("jsonView");
+        return mav;
     }
 }
