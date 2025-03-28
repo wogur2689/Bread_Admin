0df8a55ec0df6622c6ec9c9ba659f891fe0e2451
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-03-28 23:54:51 +0900

    fix : 메뉴레벨 컬럼 추가 및 미사용 로직 제거

--- src/main/java/com/example/bread/web/menu/repository/MenuRepository.java
+++ src/main/java/com/example/bread/web/menu/repository/MenuRepository.java
@@ -9,11 +9,4 @@ import java.util.List;
 
 @Repository
 public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
-
-    // 특정 메뉴의 하위 메뉴 조회
-    List<MenuEntity> findByParent(MenuEntity parent);
-
-    //Lazy Loading 방지
-    @Query("SELECT m FROM MenuEntity m LEFT JOIN FETCH m.children WHERE m.parent IS NULL")
-    List<MenuEntity> findRootMenusWithChildren();
 }
