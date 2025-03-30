1ea28770d62d57b1e31e9140f476e795d85b50a9
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-03-30 23:59:01 +0900

    refactor : maven -> gradle 전환 시작

--- src/test/java/com/hyeok/bread_admin/BreadApplicationTests.java
+++ src/test/java/com/hyeok/bread_admin/BreadApplicationTests.java
@@ -1,9 +1,10 @@
 package com.hyeok.bread_admin;
 
+import com.example.bread.BreadApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BreadApplication.class)
 class BreadApplicationTests {
 
     @Test
