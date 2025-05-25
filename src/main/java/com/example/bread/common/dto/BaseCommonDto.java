8262c6f1e812f7f88d9640a429fc4d7419404021
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-05-26 00:44:12 +0900

    feat : BaseCommonDto 생성

---
+++ src/main/java/com/example/bread/common/dto/BaseCommonDto.java
@@ -0,0 +1,24 @@
+package com.example.bread.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BaseCommonDto {
+    private LocalDateTime createdAt;    //작성일
+    private LocalDateTime updatedAt;    //수정일
+    private String createdBy;           //작성자
+    private String updatedBy;           //수정자
+
+    private String pageSize;            //갯수
+    private String pageOffset;          //페이지 번호
+}
