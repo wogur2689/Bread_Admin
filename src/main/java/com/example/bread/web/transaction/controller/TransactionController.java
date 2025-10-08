3de577d9d3f1accf99bbabdf5267f14c72dac598
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-10-08 12:22:27 +0900

    fix: 거래내역 불필요한 로직 제거

--- src/main/java/com/example/bread/web/transaction/controller/TransactionController.java
+++ src/main/java/com/example/bread/web/transaction/controller/TransactionController.java
@@ -1,11 +1,7 @@
 package com.example.bread.web.transaction.controller;
 
-import com.example.bread.common.util.CommonCode;
-import com.example.bread.web.payment.dto.PaymentDto;
-import com.example.bread.web.payment.entity.PaymentEntity;
-import com.example.bread.web.payment.service.PaymentService;
+import com.example.bread.web.transaction.entity.TransactionEntity;
 import com.example.bread.web.transaction.service.TransactionService;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -24,7 +20,7 @@ public class TransactionController {
 
     @GetMapping("/paymentList")
     public ModelAndView list(ModelAndView mav, @RequestParam(defaultValue = "1") int page) {
-        Page<TransactionEEntity> response = transactionService.list(page);
+        Page<TransactionEntity> response = transactionService.list(page);
 
         mav.addObject("response", response);
         mav.addObject("currentPage", page);
@@ -35,39 +31,8 @@ public class TransactionController {
 
     @GetMapping("/paymentView/{id}")
     public ModelAndView view(@PathVariable Long id, ModelAndView mav) {
-        mav.addObject("response", paymentService.view(id));
+        mav.addObject("response", transactionService.view(id));
         mav.setViewName("payment/payment_view");
         return mav;
-    }
-
-    @GetMapping("/paymentEdit/{id}")
-    public ModelAndView edit(@PathVariable Long id, ModelAndView mav) {
-        mav.addObject("response", paymentService.view(id));
-        mav.setViewName("payment/payment_edit");
-        return mav;
-    }
-
-    @GetMapping("/paymentWrite")
-    public ModelAndView write(ModelAndView mav) {
-        mav.setViewName("payment/payment_write");
-        return mav;
-    }
-
-    @PostMapping("/api/{svc}")
-    public ModelAndView paymentApi(@PathVariable String svc, PaymentDto.PaymentRequestDto PaymentDto, ModelAndView mav) {
-        String code = svcSwitch(svc, PaymentDto);
-        mav.addObject("code", code);
-        mav.addObject("msg", CommonCode.getMessage(code));
-        mav.setViewName("jsonView");
-        return mav;
-    }
-
-    private String svcSwitch(String svc, PaymentDto.PaymentRequestDto PaymentDto) {
-        return switch (svc) {
-            case "insert" -> paymentService.insert(PaymentDto);
-            case "update" -> paymentService.update(PaymentDto);
-            case "delete" -> paymentService.delete(PaymentDto);
-            default -> CommonCode.CODE_0000.getCode();
-        };
     }
 }
--- src/main/java/com/example/bread/web/transaction/service/TransactionService.java
+++ src/main/java/com/example/bread/web/transaction/service/TransactionService.java
@@ -1,16 +1,10 @@
 package com.example.bread.web.transaction.service;
 
-import com.example.bread.common.util.CommonCode;
 import com.example.bread.web.log.annotation.Loggable;
-import com.example.bread.web.payment.dto.PaymentDto;
-import com.example.bread.web.payment.entity.PaymentEntity;
-import com.example.bread.web.payment.repository.PaymentRepository;
-import com.example.bread.web.transaction.dto.TransactionDto;
 import com.example.bread.web.transaction.entity.TransactionEntity;
 import com.example.bread.web.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -35,38 +29,5 @@ public class TransactionService {
     @Transactional(readOnly = true)
     public TransactionEntity view(Long id) {
         return transactionRepository.findById(id).orElseThrow();
-    }
-
-    public String insert(TransactionDto.TransactionRequestDto TransactionDto) {
-        String code = CommonCode.CODE_0000.getCode();
-        try {
-            TransactionEntity transaction = TransactionEntity.toEntity(TransactionDto);
-            transactionRepository.save(transaction);
-        } catch (DataAccessException | NullPointerException e) {
-            code = "9999";
-        }
-        return code;
-    }
-
-    public String update(TransactionDto.TransactionRequestDto transactionDto) {
-        String code = CommonCode.CODE_0000.getCode();
-        try {
-            TransactionEntity transaction = TransactionEntity.toEntity(transactionDto);
-            transactionRepository.save(transaction);
-        } catch (DataAccessException | NullPointerException e) {
-            code = "9999";
-        }
-        return code;
-    }
-
-    public String delete(TransactionDto.TransactionRequestDto transactionDto) {
-        String code = CommonCode.CODE_0000.getCode();
-        try {
-            TransactionEntity Transaction = TransactionEntity.toEntity(transactionDto);
-            transactionRepository.delete(Transaction);
-        } catch (DataAccessException | NullPointerException e) {
-            code = "9999";
-        }
-        return code;
     }
 }
